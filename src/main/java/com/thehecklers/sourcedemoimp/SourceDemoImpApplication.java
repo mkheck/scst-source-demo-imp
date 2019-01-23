bf9c831547f2a9bbea821ef9146aadbea980c860
Mark Heckler <mark.heckler@gmail.com>
2019-01-22 23:00:09 -0600

    Initial commit

---
+++ src/main/java/com/thehecklers/sourcedemoimp/SourceDemoImpApplication.java
@@ -0,0 +1,49 @@
+package com.thehecklers.sourcedemoimp;
+
+import lombok.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@EnableBinding(Source.class)
+@EnableScheduling
+@SpringBootApplication
+public class SourceDemoImpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceDemoImpApplication.class, args);
+    }
+
+}
+
+@Component
+class PingSource {
+    private final Source source;
+    private boolean isA = true;
+
+    PingSource(Source source) {
+        this.source = source;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    private void ping() {
+        Ping ping = new Ping(isA ? "A" : "B", UUID.randomUUID().toString(), Instant.now().toString());
+        System.out.println(ping.toString());
+        isA = !isA;
+
+        source.output().send(MessageBuilder.withPayload(ping).build());
+    }
+}
+
+@Value
+class Ping {
+	private final String group, id, message;
+}
---
+++ src/test/java/com/thehecklers/sourcedemoimp/SourceDemoImpApplicationTests.java
@@ -0,0 +1,17 @@
+package com.thehecklers.sourcedemoimp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SourceDemoImpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
